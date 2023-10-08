b6c782338820382a0d0425feb79b260b24000a6f
sahrul30T <alsahrul060@gmail.com>
2023-10-08 23:11:49 +0800

    f

---
+++ Tugas Js/Switch Case.js
@@ -0,0 +1,15 @@
+var hari = "Selasa";
+
+switch (hari) {
+  case "Senin":
+    console.log("Hari Senin");
+    break;
+  case "Selasa":
+    console.log("Hari Selasa");
+    break;
+  case "Rabu":
+    console.log("Hari Rabu");
+    break;
+  default:
+    console.log("Hari tidak dikenali");
+}
---
+++ Tugas Js/While dan Do While.js
@@ -0,0 +1,13 @@
+var angka = 1;
+
+// While loop
+while (angka <= 5) {
+  console.log("Angka: " + angka);
+  angka++;
+}
+
+// Do while loop
+do {
+  console.log("Angka: " + angka);
+  angka++;
+} while (angka <= 5);
